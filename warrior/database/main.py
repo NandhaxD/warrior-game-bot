ddf9383972217a332810f875097725b7d4e9af34
|| ＮΛＮＤＨΛ || - நந்தா <89440790+NandhaxD@users.noreply.github.com>
2023-04-11 17:56:47 +0530

    Update main.py

--- warrior/database/main.py
+++ warrior/database/main.py
@@ -3,7 +3,7 @@ from warrior import database
 db = database["main"]
 
 async def add_users_to_db(user_id: int):
-     string = {"user_id": user_id, "coins": 500}
+     string = {"user_id": user_id, "bucks": 500}
      db.insert_one(string)
 
 async def get_users_list():
