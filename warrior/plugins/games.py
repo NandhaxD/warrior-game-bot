5eaa1ea028dca260634b81ce501d2dab97b9fa12
|| ＮΛＮＤＨΛ || - நந்தா <89440790+NandhaxD@users.noreply.github.com>
2023-04-11 22:40:11 +0530

    Update games.py

--- warrior/plugins/games.py
+++ warrior/plugins/games.py
@@ -22,22 +22,17 @@ async def dice(_, message):
     if not user_id in (await get_users_list()):
          return await ask_to_dm_first(message=message)
     if user_id in dice_users:
-        return await message.reply_text("Sorry Try Later You Already Played!")
+        return await message.reply_text("Try Later You Already Played! Next One Would Be Takes 10Minutes")
     dice_users.append(user_id)    
     xx = await bot.send_dice(chat_id=chat_id)
     value = int(xx.dice.value)
     if value == 1:
          bucks = 10000
-    elif value == 2:
-         bucks = 2000
-    elif value == 3:
-         bucks = 3000
-    elif value == 4:
-          bucks = 4000
-    elif value == 5:
-         bucks = 5000
-    elif value == 6:
-         bucks = 6000
+    else:
+         await message.reply_text("🥸 Better Luck Next Time.")
+         await asyncio.sleep(10*60)
+         dice_users.remove(user_id)
+         return 
     await add_bucks_to_db(
         user_id=user_id,
         bucks=bucks)
@@ -45,6 +40,37 @@ async def dice(_, message):
             user_id=user_id)
     await message.reply_text(
         f"You won: **{bucks}**\nTotal bucks: **{kk}**")
+    await asyncio.sleep(10*60)
+    dice_users.remove(user_id)
+    return 
+
+
+dart_users = []
+@bot.on_message(filters.command("dart", prefix))
+async def dart(_, message):
+    user_id = message.from_user.id
+    chat_id = message.chat.id
+    if not user_id in (await get_users_list()):
+         return await ask_to_dm_first(message=message)
+    if user_id in dice_users:
+        return await message.reply_text("Try Later You Already Played! Next One Would Be Takes 10Minutes")
+    dart_users.append(user_id)    
+    xx = await bot.send_dice(chat_id=chat_id, "🎯")
+    value = int(xx.dice.value)
+    if value == 6:
+         bucks = 10000
+    else:
+         await message.reply_text("🥸 Better Luck Next Time.")
+         await asyncio.sleep(10*60)
+         dart_users.remove(user_id)
+         return 
+    await add_bucks_to_db(
+        user_id=user_id,
+        bucks=bucks)
+    kk = await get_bucks_from_users(
+            user_id=user_id)
+    await message.reply_text(
+        f"You won ✅: **{bucks}**\nTotal Bucks 💰: **{kk}**")
     await asyncio.sleep(10*60)
     dice_users.remove(user_id)
     return 
