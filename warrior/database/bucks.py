01d78cc13ed4a43a6d7ce9e2dbc37e46e327b289
|| ＮΛＮＤＨΛ || - நந்தா <89440790+NandhaxD@users.noreply.github.com>
2023-04-11 18:32:01 +0530

    Update and rename coins.py to bucks.py

---
+++ warrior/database/bucks.py
@@ -0,0 +1,17 @@
+from warrior import database
+
+
+db = database["main"]
+
+
+async get_bucks_from_users(user_id: int):
+     string = {"user_id": user_id}
+     xx = db.find_one(string)
+     mm = int(xx["bucks"])
+     return mm
+
+async def add_bucks_to_db(user_id: int, bucks: int):
+        xx = await get_bucks_from_users(user_id)
+        total_bucks = xx+bucks
+        string = {"user_id": user_id, {"$set":{"bucks": total _bucks}}}
+        db.update_one(string)
--- warrior/database/coins.py
+++
@@ -1,7 +0,0 @@
-from warrior import database
-
-
-db = database["main"]
-
-async def add_bucks_to_db(user_id: int, bucks: int):
-          
