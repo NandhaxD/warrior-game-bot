3bb4aadb2ef7d0738b6ea82804c66d71cb201c53
|| ＮΛＮＤＨΛ || - நந்தா <89440790+NandhaxD@users.noreply.github.com>
2023-04-11 17:47:13 +0530

    Update main.py

--- warrior/plugins/main.py
+++ warrior/plugins/main.py
@@ -1,1 +1,19 @@
 
+
+from warrior import bot, prefix 
+from warrior.database.main import add_users_to_db, get_users_list
+from pyrogram import filters, enums 
+
+
+@bot.on_message(filters.command("start", prefix))
+async def start(_, message):
+      user_id = int(message.from_user.id)
+      if message.chat.type == enums.ChatType.PRIVATE:
+             if not user_id in await get_users_list():
+                    await add_users_to_db(user_id)
+                    return await message.reply_text("You have been added to my database. that case you got 500 coins.") 
+             else:
+                 return await message.reply_text("*you start message text here*")
+      else:
+          return await message.reply_text("*your start message text here*")
+
