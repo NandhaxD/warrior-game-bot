6745dd00d84420d88f41d507192beac65b9a5721
|| ＮΛＮＤＨΛ || - நந்தா <89440790+NandhaxD@users.noreply.github.com>
2023-04-11 22:57:12 +0530

    Update games.py

--- warrior/plugins/games.py
+++ warrior/plugins/games.py
@@ -74,3 +74,35 @@ async def dart(_, message):
     await asyncio.sleep(10*60)
     dart_users.remove(user_id)
     return 
+
+
+football_users = []
+@bot.on_message(filters.command("football", prefix))
+async def football(_, message):
+    user_id = message.from_user.id
+    chat_id = message.chat.id
+    if not user_id in (await get_users_list()):
+         return await ask_to_dm_first(message=message)
+    if user_id in football_users:
+        return await message.reply_text("Try Later You Already Played! Next One Would Be Takes 10Minutes")
+    football_users.append(user_id)    
+    xx = await bot.send_dice(chat_id=chat_id, emoji="⚽")
+    value = int(xx.dice.value)
+    if value != 1 and value != 2:
+         bucks = 10000
+    else:
+         await message.reply_text("🥸 Better Luck Next Time.")
+         await asyncio.sleep(10*60)
+         football_users.remove(user_id)
+         return 
+    await add_bucks_to_db(
+        user_id=user_id,
+        bucks=bucks)
+    kk = await get_bucks_from_users(
+            user_id=user_id)
+    await message.reply_text(
+        f"You won ✅: **{bucks}**\nTotal Bucks 💰: **{kk}**")
+    await asyncio.sleep(10*60)
+    football_users.remove(user_id)
+    return 
+
