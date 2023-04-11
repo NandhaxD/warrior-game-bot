26b784dc02c59ba53961778678a7067ec708c838
|| ＮΛＮＤＨΛ || - நந்தா <89440790+NandhaxD@users.noreply.github.com>
2023-04-11 17:08:34 +0530

    Create __init__.py

---
+++ warrior/__init__.py
@@ -0,0 +1,36 @@
+import os
+import logging
+
+
+from pyrogram import Client 
+from pymongo import MongoClient
+
+
+
+#mongo database 
+mongodb_url = "mongodb+srv://Kora:Kora@2008@kora.zbev3wd.mongodb.net/?retryWrites=true&w=majority"
+mongo = MongoClient(mongodb_url)
+database = mongo.warrior
+
+
+
+FORMAT = "[Warrior]: %(message)s"
+
+logging.basicConfig(level=logging.INFO, handlers=[logging.FileHandler('logs.txt'),
+              logging.StreamHandler()], format=FORMAT)
+
+
+API_ID = getenv("API_ID")
+API_HASH = getenv("API_HASH")
+TOKEN = getenv("TOKEN")
+
+
+
+bot = Client(
+       name="warrior"
+       api_id=API_ID,
+       api_hash=API_HASH,
+       bot_token=TOKEN,
+       plugins=dict(root="warrior"),)
+
+
