642f0793ffcbee88862dc02b91ebe6cfc74ff3ba
|| ＮΛＮＤＨΛ || - நந்தா <89440790+NandhaxD@users.noreply.github.com>
2023-04-11 19:04:13 +0530

    Update bucks.py

--- warrior/database/bucks.py
+++ warrior/database/bucks.py
@@ -13,5 +13,5 @@ async def get_bucks_from_users(user_id: int):
 async def add_bucks_to_db(user_id: int, bucks: int):
         xx = await get_bucks_from_users(user_id)
         total_bucks = xx+bucks
-        string = {"user_id": user_id, {"$set":{"bucks": total _bucks}}}
+        string = {"user_id": user_id}, {"$set":{"bucks": total _bucks}}}
         db.update_one(string)
