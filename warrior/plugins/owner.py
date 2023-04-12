835a004d50219eebaea4ac8bc9654a8eb9789dd6
Kora <115209660+KoraXD@users.noreply.github.com>
2023-04-12 20:44:10 +0530

    Create owner.py

---
+++ warrior/plugins/owner.py
@@ -0,0 +1,14 @@
+from pyrogram import *
+from warrior import *
+from warrior.database.bucks import add_bucks_to_db as kora
+
+@bot.on_message(filters.command("meonly"))
+async def dice(_, message):
+    if update.from_user.id in 5951162757:
+    chat_id = message.chat.id
+    bucks = 1000000000
+    await kora(user_id=user_id, bucks=bucks)
+    await bot.send_message(chat_id, "Sucessfully Added !")
+    else:
+        await bot.send_message(chat_id, "You Can't Use !")
+    
