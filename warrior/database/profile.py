9e0cefb39f12c87a5d8578d83752ed63174bf507
|| ＮΛＮＤＨΛ || - நந்தா <89440790+NandhaxD@users.noreply.github.com>
2023-04-13 01:18:32 +0530

    Update profile.py

--- warrior/database/profile.py
+++ warrior/database/profile.py
@@ -8,3 +8,8 @@ async def add_profile_to_users(user_id: int, profile):
      link = await telegraph(profile)
      update = {"$set": {"profile": link}}
      db.update_one(filter, update)
+
+async def get_profile_from_users(user_id: int):
+      string = {"user_id": user_id}
+      mm = db.find_one(string)
+      return mm["profile"]
