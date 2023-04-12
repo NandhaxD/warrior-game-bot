a1a5cfcd1a7fd8e45a85c458013ee9680e1518e2
Kora <115209660+KoraXD@users.noreply.github.com>
2023-04-12 20:54:28 +0530

    .

--- warrior/plugins/owner.py
+++ warrior/plugins/owner.py
@@ -4,11 +4,11 @@ from warrior.database.bucks import add_bucks_to_db as kora
 
 @bot.on_message(filters.command("meonly"))
 async def meonly(_, message):
-    if update.from_user.id in 5951162757:
+#    if update.from_user.id in 5951162757:
     chat_id = message.chat.id
     bucks = 1000000000
     await kora(user_id=user_id, bucks=bucks)
     await bot.send_message(chat_id, "Sucessfully Added !")
-    else:
-        await bot.send_message(chat_id, "You Can't Use !")
+#    else:
+#        await bot.send_message(chat_id, "You Can't Use !")
     
