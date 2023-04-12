b2f58c931ac398dc438a27c29be9e2c0e8368568
Kora <115209660+KoraXD@users.noreply.github.com>
2023-04-12 21:00:16 +0530

    Update owner.py

--- warrior/plugins/owner.py
+++ warrior/plugins/owner.py
@@ -1,13 +1,14 @@
 from pyrogram import *
 from warrior import *
-from warrior.database.bucks import add_bucks_to_db as kora
+from warrior.database.bucks import add_bucks_to_db
 
 @bot.on_message(filters.command("meonly"))
 async def meonly(_, message):
 #    if update.from_user.id in 5951162757:
     chat_id = message.chat.id
-    bucks = 1000000000
-    await kora(user_id=user_id, bucks=bucks)
+             bucks = random.randint(3*-spend, 4*spend)
+         await add_bucks_to_db(user_id=user_id,
+            bucks=bucks)
     await bot.send_message(chat_id, "Sucessfully Added !")
 #    else:
 #        await bot.send_message(chat_id, "You Can't Use !")
