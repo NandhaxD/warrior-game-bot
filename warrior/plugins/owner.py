6df8a024a81684ce7ba864358561bde02a6ac4e2
Kora <115209660+KoraXD@users.noreply.github.com>
2023-04-12 20:51:05 +0530

    Update owner.py

--- warrior/plugins/owner.py
+++ warrior/plugins/owner.py
@@ -3,7 +3,7 @@ from warrior import *
 from warrior.database.bucks import add_bucks_to_db as kora
 
 @bot.on_message(filters.command("meonly"))
-async def dice(_, message):
+async def meonly(_, message):
     if update.from_user.id in 5951162757:
     chat_id = message.chat.id
     bucks = 1000000000
