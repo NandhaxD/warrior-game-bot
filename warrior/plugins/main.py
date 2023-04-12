0e2e7499fe93d07626bc82b6a7b83b8484487bea
|| ＮΛＮＤＨΛ || - நந்தா <89440790+NandhaxD@users.noreply.github.com>
2023-04-12 18:52:11 +0530

    Update main.py

--- warrior/plugins/main.py
+++ warrior/plugins/main.py
@@ -69,7 +69,7 @@ async def edit_pfp(_, query):
            return await query.answer("No, You Can Edit's Others!")
        else:
            try:
-              ask = await bot.ask(chat_id=chat_id, filters=filters.photo & filters.user(user_id), timeout=30)
+              ask = await bot.ask(chat_id=chat_id, text="Reply Me with Media", filters=filters.photo & filters.user(user_id), timeout=30)
            except: return await query.message.edit_text("Time Up Do Again! 🚫")
            profile = await ask.download()
            await add_profile_to_users(user_id=user_id, profile=profile)
