186df5249500ae33392b19c1659278dd560c77c6
|| ＮΛＮＤＨΛ || - நந்தா <89440790+NandhaxD@users.noreply.github.com>
2023-04-12 09:56:02 +0530

    Update telegraph.py

--- warrior/helper/telegraph.py
+++ warrior/helper/telegraph.py
@@ -1,1 +1,8 @@
 
+from telegraph import upload_file
+
+async def telegraph(path):
+     telegraph = await upload_file(path)
+     for file_id in telegraph:
+          url = "https://graph.org/" + file_id
+     return url
