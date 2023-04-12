f3fca06e1f1a4129c9c3dd20e193f2828255b13c
|| ＮΛＮＤＨΛ || - நந்தா <89440790+NandhaxD@users.noreply.github.com>
2023-04-12 08:59:46 +0530

    Update games.py

--- warrior/plugins/games.py
+++ warrior/plugins/games.py
@@ -130,3 +130,33 @@ async def basketball(_, message):
     basketball_users.remove(user_id)
     return 
 
+
+bowling_users = []
+@bot.on_message(filters.command("bowling", prefix))
+async def bowling(_, message):
+    user_id = message.from_user.id
+    chat_id = message.chat.id
+    if not user_id in (await get_users_list()):
+         return await ask_to_dm_first(message=message)
+    if user_id in bowling_users:
+        return await message.reply_text("Try Later You Already Played! Next One Would Be Takes 10Minutes")
+    bowling_users.append(user_id)    
+    xx = await bot.send_dice(chat_id=chat_id, emoji="🏀")
+    value = int(xx.dice.value)
+    if value == 6:
+         bucks = 10000
+    else:
+         await message.reply_text("🥸 Better Luck Next Time.")
+         await asyncio.sleep(10*60)
+         bowling_users.remove(user_id)
+         return 
+    await add_bucks_to_db(
+        user_id=user_id,
+        bucks=bucks)
+    kk = await get_bucks_from_users(
+            user_id=user_id)
+    await message.reply_text(
+        f"You won ✅: **{bucks}**\nTotal Bucks 💰: **{kk}**")
+    await asyncio.sleep(10*60)
+    bowling_users.remove(user_id)
+    return 
