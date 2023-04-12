5471f1fdbbd1d8ae7420db152ad01d6dcc2a05d5
|| ＮΛＮＤＨΛ || - நந்தா <89440790+NandhaxD@users.noreply.github.com>
2023-04-12 19:29:50 +0530

    Update telegraph.py

--- warrior/helper/telegraph.py
+++ warrior/helper/telegraph.py
@@ -2,7 +2,7 @@
 from telegraph import upload_file
 
 async def telegraph(path):
-     telegraph = await upload_file(path)
+     telegraph = upload_file(path)
      for file_id in telegraph:
           url = "https://graph.org/" + file_id
      return url
