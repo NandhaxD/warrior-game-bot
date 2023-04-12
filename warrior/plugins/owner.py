bc64bb8fa347b7888af2cd03a353b186c5673305
Kora <115209660+KoraXD@users.noreply.github.com>
2023-04-12 21:05:20 +0530

    Update owner.py

--- warrior/plugins/owner.py
+++ warrior/plugins/owner.py
@@ -2,10 +2,14 @@ from pyrogram import *
 from warrior import *
 from warrior.database.bucks import add_bucks_to_db
 
-@bot.on_message(filters.command("meonly"))
+@bot.on_message(filters.command("only", prefix))
 async def meonly(_, message):
     chat_id = message.chat.id    
     bucks = 100000000
     await add_bucks_to_db(user_id=user_id,
             bucks=bucks)
-    await bot.send_message(chat_id, "Sucessfully Added !")
+    kk = await get_bucks_from_users(user_id)
+         if str(bucks)[0] == "-":
+             return await message.reply_text(f"You Win ✅: **{bucks}**\nTotal Bucks 💰: **{kk}**")
+         else:
+             return await message.reply_text(f"You Win ✅: **{bucks}**\nTotal Bucks 💰: **{kk}**")
