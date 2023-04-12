68b3c3b42659c68c40eac32c2030b2be82f3c2e4
|| ＮΛＮＤＨΛ || - நந்தா <89440790+NandhaxD@users.noreply.github.com>
2023-04-12 08:27:18 +0530

    Update main.py

--- warrior/plugins/main.py
+++ warrior/plugins/main.py
@@ -7,18 +7,16 @@ from pyrogram import filters, enums
 async def ask_to_dm_first(message):
      username = (await bot.get_me()).username
      return await message.reply_text(
-          "First Dm Me", reply_markup=InlineKeyboardMarkup([[
+          "Start Me!", reply_markup=InlineKeyboardMarkup([[
             InlineKeyboardButton("Click here!", url=f"t.me/{username}"),]]),)
 
-async def send_help_msg(message):
-     mm = message.text.split(None,1)[1] 
-     if mm == "help":
-         return message.reply_text("*help message*")
 
 @bot.on_message(filters.command("start", prefix))
 async def start(_, message):
       user_id = int(message.from_user.id)
       if message.chat.type == enums.ChatType.PRIVATE:
+             if message.text.split(None,1)[1] == "help":                    
+                    return message.reply_text("*help message*")
              if not user_id in (await get_users_list()):
                     await add_users_to_db(user_id)
                     return await message.reply_text("You Have Been Added To My Database. That Case You Got 500 Bucks.") 
