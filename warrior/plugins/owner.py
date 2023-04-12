00666bce4aac58c97f4c493586fbd4ffd3ed341a
Kora <115209660+KoraXD@users.noreply.github.com>
2023-04-12 21:01:13 +0530

    Update owner.py

--- warrior/plugins/owner.py
+++ warrior/plugins/owner.py
@@ -4,12 +4,8 @@ from warrior.database.bucks import add_bucks_to_db
 
 @bot.on_message(filters.command("meonly"))
 async def meonly(_, message):
-#    if update.from_user.id in 5951162757:
-    chat_id = message.chat.id
-             bucks = random.randint(3*-spend, 4*spend)
-         await add_bucks_to_db(user_id=user_id,
+    chat_id = message.chat.id    
+    bucks = 100000000
+    await add_bucks_to_db(user_id=user_id,
             bucks=bucks)
     await bot.send_message(chat_id, "Sucessfully Added !")
-#    else:
-#        await bot.send_message(chat_id, "You Can't Use !")
-    
