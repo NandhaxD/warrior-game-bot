f419af47888fb284eccb9448990eaf46cfa18227
|| ＮΛＮＤＨΛ || - நந்தா <89440790+NandhaxD@users.noreply.github.com>
2023-04-13 07:59:50 +0530

    Update profile.py

--- warrior/database/profile.py
+++ warrior/database/profile.py
@@ -3,7 +3,7 @@ from warrior.helper.telegraph import telegraph
 
 db = DATABASE["MAIN"]
 
-async def add_profile_to_users(user_id: int, profile):
+async def add_profile_to_users(message, user_id: int, profile):
      filter = {"user_id": user_id}
      link = await telegraph(profile)
      update = {"$set": {"profile": link}}
