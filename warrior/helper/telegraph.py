5f5d21a2502c72c71df83eb01076acbdb8767c0a
|| ＮΛＮＤＨΛ || - நந்தா <89440790+NandhaxD@users.noreply.github.com>
2023-04-13 09:35:43 +0530

    Update telegraph.py

--- warrior/helper/telegraph.py
+++ warrior/helper/telegraph.py
@@ -4,8 +4,13 @@ from telegraph import upload_file
 async def telegraph(message, path):
      try:
        telegraph = upload_file(path)
-     except JSONDecodeError:
-             return await message.reply_text("Failed To Upload Profile In Telegraph. 🚫")
-     for file_id in telegraph:
-          url = "https://graph.org/" + file_id
+     except JSONDecodeError:        
+             await message.reply_text("Failed To Upload 🚫.")
+             url = False
+             return url
+     try:
+        for file_id in telegraph:
+             url = "https://graph.org/" + file_id
+     except:
+         pass
      return url
