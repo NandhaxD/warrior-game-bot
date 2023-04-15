0877bfae35e343d7b9b802b55520b311c6ff3367
|| ＮΛＮＤＨΛ || - நந்தா <89440790+NandhaxD@users.noreply.github.com>
2023-04-15 12:46:09 +0530

    Update code_generate.py

--- warrior/helper/code_generate.py
+++ warrior/helper/code_generate.py
@@ -2,7 +2,7 @@
 import random
 import string
 
-def generate_random_string(10):
-    code = random.choices(string.printable, k=length).encode('utf-8').decode()
+def generate_random_string():
+    code = random.choices(string.printable, k=10).encode('utf-8').decode()
     return code.replace(" ", "")
 
