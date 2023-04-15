8cced60e5b1e2ca0ed60e5dc47c2513ff98c6a58
|| ＮΛＮＤＨΛ || - நந்தா <89440790+NandhaxD@users.noreply.github.com>
2023-04-15 12:56:18 +0530

    Create lottery.py

---
+++ warrior/database/lottery.py
@@ -0,0 +1,27 @@
+from warrior import DATABASE
+from warrior.helper.code_generate import generate_random_string 
+
+db = DATABASE["LOTTERY"]
+
+
+
+async def add_lottery_to_db(bucks: int): 
+    code = generate_random_string()   
+    string = {"code": code, "bucks": bucks}
+    db.insert_one(string)
+
+
+async def remove_lotter_to_db(code: str):
+      string = {"code": code}
+      find = db.find_one(string)
+      if find:
+          db.delete_one(find)
+          return True 
+      else: 
+         return False
+
+
+async def get_lottery_code():
+      code = [x["code"] for x in db.find()]
+      return code
+    
