bd87584b034b4ac98023a0b6ff7c44d978c090f9
|| ＮΛＮＤＨΛ || - நந்தா <89440790+NandhaxD@users.noreply.github.com>
2023-04-16 09:23:46 +0530

    Update theft.py

--- warrior/database/theft.py
+++ warrior/database/theft.py
@@ -2,6 +2,17 @@ from warrior import DATABASE
 db = DATABASE["THEFT"]
 
 
+async def theft_edit(x, symbol):
+    length = len(symbol)
+    for i in range(length):
+           new_symbol = "⚉" * (length - i - 1) + symbol[i] + "⚉" * i
+           await x.edit(new_symbol)
+           await asyncio.sleep(2)
+
+
+y = await message.reply("oiii")
+await edit(x=y, symbol="⚇⚇⚇⚇⚇⚇⚇⚇⚇")
+
 async def add_user_has_theft_to_user(user_id: int, theft_user_id: int):
         ff = db.find_one({"user_id": user_id})
         if ff:
